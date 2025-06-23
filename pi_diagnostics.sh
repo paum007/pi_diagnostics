064091a2fa9503671007625b43b8f68fd3437d89
paum007 <paumaria07@gmail.com>
2025-06-23 11:50:48 +0100

    There have been warning messages added if the Pi's health is at risk

--- pi_diagnostics.sh
+++ pi_diagnostics.sh
@@ -18,7 +18,6 @@ log() {
   esac
 }
 
-echo "Logging system metrics to $log_data..."
 echo "Press ctr + C to stop the process"
 
 log "---------------------------------------"
@@ -31,10 +30,30 @@ do
 
     log "Timestamp: $(date)"
     log "IP Address: $(hostname -I)"
-    log "CPU Temp: $(vcgencmd measure_temp)"
-    log "CPU Usage: $(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')%"
+
+    temp = $(vcgencmd measure_temp)
+    log "CPU Temp: ${temp}ºC"
+
+    if (( $(echo "$temp > 70.0" | bc -1))); then 
+      log "[WARNING] CPU temperature is too high!"
+    fi
+
+    cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+    log "CPU Usage: ${cpu_usage}%"
+
+    if (( $(echo "$cpu_usage > 80.0" | bc -1) )); then
+      log "[WARNING] CPU usage is above 80%!"
+    fi
+
     log "CPU Frequency: $(vcgencmd measure_clock arm | awk -F"=" '{printf "%.2f MHz\n", $2 / 1000000}')"
-    log "Throttle Status: $(vcgencmd get_throttled)"
+
+    throttle_status=$(vcgencmd get_throttled)
+    log "Throttle Status: $throttle_status"
+    
+    if [[ "$throttle_status" != "throttled=0x0" ]]; then
+        log "[WARNING] Throttling detected (under-voltage or overheating)!"
+    fi
+
     log "Load Average: $(uptime | awk -F'load average:' '{ print $2 }')"
     log "GPU Memory: $(vcgencmd get_mem gpu)"
 
@@ -43,8 +62,13 @@ do
     free -h
 
     log "---"
-    log "Disk Usage:"
-    df -h /
+
+    disk_usage=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
+    log "Disk Usage: ${disk_usage}%"
+
+    if [ "$disk_usage" -gt 90 ]; then
+        log "[WARNING] Disk usage above 90%!"
+    fi
 
     log "-----------------------------"
   sleep 10
