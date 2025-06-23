df5e7f040039a9b2730463c7bdf866269f4a7703
paum007 <paumaria07@gmail.com>
2025-06-23 11:33:20 +0100

    Made it so that the user can choose between displaying the data to the terminal, write it directly to the file or both

--- pi_diagnostics.sh
+++ pi_diagnostics.sh
@@ -2,34 +2,51 @@
 
 log_data=~/pi_diagnostics/system_log.txt
 
+echo "Select output mode: "
+echo "[1] Write the data to file only"
+echo "[2] Display the data to the terminal only"
+echo "[3] Do both of the above options"
+
+read -p "Enter choise [1-3]: " output_mode
+
+log() {
+  case $output_mode in
+    1) echo "$1" >> "$log_data" ;;
+    2) echo "$1" ;;
+    3) echo "$1" | tee -a "$log_data" ;;
+    *) echo "$1" ;;
+  esac
+}
+
 echo "Logging system metrics to $log_data..."
 echo "Press ctr + C to stop the process"
-echo "---------------------------------------" >> "$log_data"
-echo "System Monitoring Started: $(date)" >> "$log_data"
-echo "---------------------------------------" >> "$log_data"
+
+log "---------------------------------------"
+log "System Monitoring Started: $(date)"
+log "---------------------------------------"
 
 while true
 do
-    echo "" >> "$log_data"
+    log ""
 
-    echo "Timestamp: $(date)" >> "$log_data"
-    echo "IP Address: $(hostname -I)" >> "$log_data"
-    echo "CPU Temp: $(vcgencmd measure_temp)" >> "$log_data"
-    echo "CPU Usage: $(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')%" >> "$log_data"
-    echo "CPU Frequency: $(vcgencmd measure_clock arm | awk -F"=" '{printf "%.2f MHz\n", $2 / 1000000}')" >> "$log_data"
-    echo "Throttle Status: $(vcgencmd get_throttled)" >> "$log_data"
-    echo "Load Average: $(uptime | awk -F'load average:' '{ print $2 }')" >> "$log_data"
-    echo "GPU Memory: $(vcgencmd get_mem gpu)" >> "$log_data"
+    log "Timestamp: $(date)"
+    log "IP Address: $(hostname -I)"
+    log "CPU Temp: $(vcgencmd measure_temp)"
+    log "CPU Usage: $(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')%"
+    log "CPU Frequency: $(vcgencmd measure_clock arm | awk -F"=" '{printf "%.2f MHz\n", $2 / 1000000}')"
+    log "Throttle Status: $(vcgencmd get_throttled)"
+    log "Load Average: $(uptime | awk -F'load average:' '{ print $2 }')"
+    log "GPU Memory: $(vcgencmd get_mem gpu)"
 
-    echo "---" >> "$log_data"
-    echo "Memory Usage:" >> "$log_data"
-    free -h >> "$log_data"
+    log "---"
+    log "Memory Usage:"
+    free -h
 
-    echo "---" >> "$log_data"
-    echo "Disk Usage:" >> "$log_data"
-    df -h / >> "$log_data"
+    log "---"
+    log "Disk Usage:"
+    df -h /
 
-    echo "-----------------------------" >> "$log_data"
+    log "-----------------------------"
   sleep 10
 done
 
